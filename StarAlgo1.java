c118a4870e0c3c3dc8a18e722fad7e524b41f351
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-22 13:20:31 +0530

    Implement Algo

---
+++ StarAlgo1.java
@@ -0,0 +1,10 @@
+public class StarAlgo1{
+    public static void main (String args[]){
+        for (int y = 0; y < 4; y++) {
+            for(int x = 0; x < 4; x++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
