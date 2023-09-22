e6c1a9c661f53034616a4fc23effbd395a590980
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-22 13:36:06 +0530

    Fix the algo issue

--- StarAlgo2.java
+++ StarAlgo2.java
@@ -1,7 +1,7 @@
 public class StarAlgo2{
     public static void main(String args[]){
         for(int y = 0; y < 5; y++){
-            for(int x = 4; x >=0; x--) {
+            for(int x = 4; x >=y; x--) {
                 System.out.print("* ");
             }
             System.out.println();
