05e8ac23076165dd889837fdb5a03afe68ac59f8
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-09-22 13:30:44 +0530

    Add StarAlgo2.java

---
+++ StarAlgo2.java
@@ -0,0 +1,10 @@
+public class StarAlgo2{
+    public static void main(String args[]){
+        for(int y = 0; y < 5; y++){
+            for(int x = 4; x >=0; x--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
